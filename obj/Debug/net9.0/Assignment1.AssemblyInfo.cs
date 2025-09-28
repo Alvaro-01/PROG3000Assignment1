17750407eb471335c83e0c38ce036ea585adae7b
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2025-09-27 21:17:11 -0400

    Fixed wording

--- obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+060f0e5a8cdb59c231189c85789b877e68b79c9a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9c1a144f840e81f950f13877f2997062faca66")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
