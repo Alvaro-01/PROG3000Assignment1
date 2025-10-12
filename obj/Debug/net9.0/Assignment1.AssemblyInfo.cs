a51ed83375912269cea4ebdf3abd223d9ce201be
Alvaro-01 <alvaro.aguirremeza@georgebrown.ca>
2025-10-11 21:19:40 -0400

    rewrote how some data looked for better demsotration

--- obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/Assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a73431a362ecd56748cfddc95fe73dae860237e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8fd5fe557b237f9baf7398d4d111ff7a998c744")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
